d6111cd22807787ee92dd824e8939b62865b9bb9
Strabatras <strabatras@gmail.com>
2019-08-07 21:23:10 +0300

    Helpers добавлена функция получения sha1 хэша строки

--- helpers/helper.go
+++ helpers/helper.go
@@ -1,6 +1,10 @@
 package helpers
 
-import "regexp"
+import (
+	"crypto/sha1"
+	"encoding/hex"
+	"regexp"
+)
 
 // Поиск совпадений
 func Matched( pattern string, text string ) bool {
@@ -9,4 +13,11 @@ func Matched( pattern string, text string ) bool {
 		return true;
 	}
 	return false;
+}
+
+// Хэш строки sha1
+func Hash( str string ) string  {
+	sha := sha1.New();
+	sha.Write( []byte( str ) );
+	return hex.EncodeToString( sha.Sum( nil ) );
 }
